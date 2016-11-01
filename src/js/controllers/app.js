58f54c21a47f0726a9abbfaeacdff5a16253a413
Brit Butler <redline6561@gmail.com>
2016-11-01 12:34:06 -0400

    Add notes about the value of this and knowing when you need to bind.

--- src/js/controllers/app.js
+++ src/js/controllers/app.js
@@ -10,6 +10,13 @@ class AppController {
 
   start () {
     this.form.submit(this.createTask.bind(this));
+    // the value of this in a javascript function depends completely on the syntax of *the place it was _called_*
+    // this.createTask(); // <- inside createTask, this === AppController instance
+    // var foo = this.createTask;
+    // foo(); // <- inside foo, this === undefined
+    // Why? The value of this depends completely on whether the place the method was called looks like
+    // "foo()" or "obj.foo()". With callbacks, you should always assume it is the former and so you have
+    // to `bind(this)` to make sure you get the right value.
   }
 
   createTask (event) {
