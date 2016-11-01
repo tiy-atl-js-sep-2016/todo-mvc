64b98be68885b476eb0c44141fdb57cfe48c3c3d
Brit Butler <redline6561@gmail.com>
2016-11-01 12:32:51 -0400

    Add an example to show off the debugger.

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,18 @@ var display = $(".todo-list");
 
 var app = new AppController(list, form, display);
 app.start();
+
+// function example () {
+//   var nums = [1,2,3,4,6,7,5];
+//   var total = 0;
+//   for (var i = 0; i < nums.length; i++) {
+//     debugger;
+//     var number = nums[i];
+//     if (number % 2 === 0) {
+//       total += number;
+//     }
+//   }
+//   return total;
+// }
+
+// example();
